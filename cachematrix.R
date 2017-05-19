88e63da2cfbf018b25fa6ac9e3587f2b9a202f0b
Chris <allen.christopher89@gmail.com>
2017-05-19 13:49:39 -0600

    Added assingment function

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      A <- NULL 
+      set <- function(y) {
+            x <<- y
+            A <<- NULL
+      }
+      get <- function() x
+      setInverse <- function() A <<- solve(x) 
+      getInverse <- function() A
+      list(set = set,
+           get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Establishes idienties that can be pulled from ouside the function evniroment and caches the matix and its inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inv <- x$getInverse()
+      if (!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      mat <- x$get()
+      inv <- solve(mat, ...)
+      x$setInverse(inv)
+      inv
+      ##Checks the cache for the matrix and eitehr returns the sloved matrix, or calcuates it. 
 }
